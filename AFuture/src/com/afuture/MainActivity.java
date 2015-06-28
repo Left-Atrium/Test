86c458af5b1753ac877524cd93971f1fa090e581
Left-Atrium <tao_90@163.com>
2015-06-28 09:52:25 +0800

    Signed-off-by: Left-Atrium <tao_90@163.com>

--- AFuture/src/com/afuture/MainActivity.java
+++ AFuture/src/com/afuture/MainActivity.java
@@ -1,6 +1,5 @@
 package com.afuture;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,32 +7,48 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import com.afuture.SaveInfo.QQ;
 import com.afuture.listview.ListViewActivity;
 
 import dialog.ProgressDialogActivity;
 
 public class MainActivity extends Activity {
 	Button listview;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		listview = (Button)findViewById(R.id.listview);
+		listview = (Button) findViewById(R.id.listview);
+		// listView的测试界面
 		listview.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				Intent listViewIntent = new Intent(MainActivity.this,ListViewActivity.class);
+				Intent listViewIntent = new Intent(MainActivity.this,
+						ListViewActivity.class);
 				startActivity(listViewIntent);
 			}
 		});
+		// Dialog的测试界面
 		findViewById(R.id.Dialog).setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				startActivity(new Intent(MainActivity.this,ProgressDialogActivity.class));
+				startActivity(new Intent(MainActivity.this,
+						ProgressDialogActivity.class));
 			}
 		});
+		// 数据保存QQ的测试界面
+		findViewById(R.id.QQ).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this,
+						QQ.class));
+			}
+		});
+
 	}
 
 }
--- AFuture/src/dialog/ProgressDialogActivity.java
+++ AFuture/src/dialog/ProgressDialogActivity.java
@@ -2,6 +2,8 @@ package dialog;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -9,6 +11,7 @@ import android.view.View.OnClickListener;
 import com.afuture.R;
 
 public class ProgressDialogActivity extends Activity {
+	ProgressDialog dialog;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,9 +20,69 @@ public class ProgressDialogActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				ProgressDialog.show(ProgressDialogActivity.this, 
-						"请稍后……", "请稍后……", true);
+				dialog = new ProgressDialog(ProgressDialogActivity.this);
+				dialog.setMessage("拼命加载中……");
+				dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+				dialog.setMax(100);
+				dialog.setCancelable(false);
+				dialog.show();
+				new ProgressAsy().execute(ProgressDialogActivity.this);
 			}
 		});
 	}
+	
+	class ProgressAsy extends AsyncTask<Context, Integer, String>{
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			System.out.println("开始啦！");
+		}
+		
+		
+		
+		@Override
+		protected String doInBackground(Context... params) {
+			for(int i = 1 ; i <= 10; i++ )
+			try {
+				Thread.sleep(1000);
+				publishProgress(i*10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+		
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			dialog.setProgress(values[0]);
+			if(100 == values[0])
+				dialog.dismiss();
+			super.onProgressUpdate(values);
+			
+			
+		}
+		
+		
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
